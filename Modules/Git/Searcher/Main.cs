be09c1b65cfc434d90663a2ac0afc80aae47dcbd
Falcion <57592842+Falcion@users.noreply.github.com>
2020-09-05 19:24:17 +0300

    Added new command: search-code.
    1.) Added new requester: code requester (special parser for new command).
    2.) Added new command: search-code.

--- Modules/Git/Searcher/Main.cs
+++ Modules/Git/Searcher/Main.cs
@@ -332,5 +332,135 @@ namespace Stratum {
             await Context.Channel.SendMessageAsync("", false,
                                                         messageEmbed.Build()    );
         }
+
+        [Command("search-code")]
+
+        public async Task SearchCode(int page, [Remainder]string filters) {
+
+            string apiToken
+                    = Storage.apiToken;
+
+            GitHubClient gitClient
+                    = new GitHubClient(new ProductHeaderValue("Stratum"));
+
+            Credentials tokenAuth
+                        = new Credentials(apiToken);
+
+            gitClient.Credentials = tokenAuth;
+            
+            string filename = "none",
+                   path = "none",
+                   user = "none";
+
+            string reposOwner = "none",
+                   reposName = "none";
+
+            Language language = Language.Unknown;
+
+            bool forks = false;
+
+            Octokit.Range sizeRange = Octokit.Range.GreaterThan(0);
+
+            string[] filterArray = filters.Split("/ +/");
+
+            for(int i = 0; i < filterArray.Length; i++) {
+
+                    if(filterArray[i].StartsWith(" filename: ")) {
+                        
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 7);
+
+                        filename = filterArray[i];
+                    }
+
+                    if(filterArray[i].StartsWith(" path: ")) {
+
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 7);
+
+                        path = filterArray[i];
+                    }
+
+                    if(filterArray[i].StartsWith(" language: ")) {
+
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 11);
+
+                        language
+                                = new LanguageRequester().LanguageRegister(filterArray[i]);
+                    }
+
+                    if(filterArray[i].StartsWith(" forks: ")) {
+                        
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 8);
+
+                        forks = bool.Parse(filterArray[i]);
+                    }
+
+                    if(filterArray[i].StartsWith(" size: ")) {
+
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 7);
+
+                        string[] indexer = filterArray[i].Split(' ');
+
+                        sizeRange
+                                = new SizeRequester().SizeRegister(indexer[0], indexer[1]);
+                    }
+
+                    if(filterArray[i].StartsWith(" user: ")) {
+                        
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 7);
+
+                        user = filterArray[i];
+                    }
+
+                    if(filterArray[i].StartsWith(" repos-link: ")) {
+                        
+                        filterArray[i]
+                                = filterArray[i].Remove(0, 7);
+
+                        string[] reposArray = filterArray[i].Split('/');
+
+                        reposOwner = reposArray[0];
+                        reposName = reposArray[1];
+                    }
+            }
+
+            SearchCodeRequest codeRequest
+                = new CodeRequester().CodeRegister(filename, path, language, forks, sizeRange, user, reposOwner, reposName);
+
+            SearchCodeResult codeResult
+                = await gitClient.Search.SearchCode(codeRequest);
+
+            EmbedBuilder messageEmbed = new EmbedBuilder()
+
+                                                        .WithTitle("Search Code Request - Result")
+                                                        .WithDescription("Current list's count: " + codeResult.TotalCount + ". If you want to see all of users, please type next page's number.")
+                                                        .WithColor(Color.Default)
+                                                        .WithCurrentTimestamp()
+                                                        .WithFooter(
+                                                            footer => footer.Text = "Page: " + page.ToString()
+                                                        );
+
+            if(page > 0) page--;
+            else if(page <= 0) page = 0;
+
+            int algBreaker = 0;
+            for(int i = 0 + 25 * page; i < codeResult.Items.Count; i++) {
+                
+                algBreaker++;
+                if(algBreaker == 25) break;
+
+                string reposInfo = "SHA-code: ``" + codeResult.Items[i].Sha + "``\nPath: ``" + codeResult.Items[i].Path + "``\nURL: " + codeResult.Items[i].Url + "``";
+
+                messageEmbed.AddField(codeResult.Items[i].Name, reposInfo);
+            }
+
+            await Context.Channel.SendMessageAsync("", false,
+                                                        messageEmbed.Build()    );
+        }
     }
 }
