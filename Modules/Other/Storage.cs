42d143a4a3e37c8b609144d5acc31b428f65d74f
Falcion <57592842+Falcion@users.noreply.github.com>
2020-10-07 14:37:32 +0300

    Preparing repository for full rework.

--- Modules/Other/Storage.cs
+++
@@ -1,9 +0,0 @@
-namespace Stratum {
-
-    internal static class Storage {
-        
-        internal static string authToken;
-        internal static string apiToken;
-        internal static string prefix;
-    }
-}
