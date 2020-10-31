6c96fb77cc4fe94ba5cb7102ecb0f7f18981645a
Falcion <57592842+Falcion@users.noreply.github.com>
2020-11-01 01:55:12 +0300

    Added core of bot.
    1.) Includes custom logger.
    2.) Includes custom configuration module.
    3.) Added core of bot with error response.

---
+++ Configuration.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+
+namespace Stratum
+{
+    public static class Configuration
+    {
+        public static string AUTH_TOKEN;
+        public static string GIT_TOKEN;
+        public static string BOT_PREFIX;
+
+        public static void Init()
+        {
+            if(!File.Exists(".cfg"))
+            {
+                File.Create(".cfg").Close();
+
+                Logger.Warn("Created configuration file! Write GitHub API token and Discord API token in this file.");
+
+                string ARRAY = "AUTH_TOKEN: \"\"\nGIT_TOKEN: \"\"\nBOT_PREFIX: \"$ \"";
+
+                File.WriteAllText(".cfg", ARRAY);
+
+                Console.ForegroundColor = ConsoleColor.Black;
+                Environment.Exit(-1);
+            }
+
+            PostInit();
+        }
+
+        public static void PostInit()
+        {
+            string[] param = File.ReadAllLines(".cfg");
+
+            for (int i = 0; i < param.Length; i++)
+            {
+                if (param[i].StartsWith("AUTH_TOKEN: "))
+                {
+                    param[i] = param[i].Remove(0, 12);
+
+                    param[i] = param[i].Remove(param[i].IndexOf('\"'), 1);
+                    param[i] = param[i].Remove(param[i].LastIndexOf('\"'), 1);
+
+                    AUTH_TOKEN = param[i];
+                }
+                if (param[i].StartsWith("GIT_TOKEN: "))
+                {
+                    param[i] = param[i].Remove(0, 11);
+
+                    param[i] = param[i].Remove(param[i].IndexOf('\"'), 1);
+                    param[i] = param[i].Remove(param[i].LastIndexOf('\"'), 1);
+
+                    GIT_TOKEN = param[i];
+                }
+                if (param[i].StartsWith("BOT_PREFIX: "))
+                {
+                    param[i] = param[i].Remove(0, 12);
+
+                    param[i] = param[i].Remove(param[i].IndexOf('\"'), 1);
+                    param[i] = param[i].Remove(param[i].LastIndexOf('\"'), 1);
+
+                    BOT_PREFIX = param[i];
+                }
+            }
+        }
+
+        public static string getAuthToken()
+        {
+            return AUTH_TOKEN;
+        }
+
+        public static void setAuthToken(string token)
+        {
+            AUTH_TOKEN = token;
+        }
+
+        public static string getGitToken()
+        {
+            return GIT_TOKEN;
+        }
+
+        public static void setGitToken(string token)
+        {
+            GIT_TOKEN = token;
+        }
+
+        public static string getBotPrefix()
+        {
+            return BOT_PREFIX;
+        }
+
+        public static void setBotPrefix(string prefix)
+        {
+            BOT_PREFIX = prefix;
+        }
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stratum
+{
+    public static class Logger
+    {
+        public static void Info(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.WriteLine($"[{DateTime.Now}]: " + message);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void Warn(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"[{DateTime.Now}]: " + message);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        public static void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine($"[{DateTime.Now}]: " + message);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
