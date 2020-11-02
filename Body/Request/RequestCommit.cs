ff80e9f88ce671bc2c14c6a9c0959ad03f262bfa
Falcion <57592842+Falcion@users.noreply.github.com>
2020-11-02 04:26:05 +0300

    Fixed comments in RequestCommit.cs

--- Body/Request/RequestCommit.cs
+++ Body/Request/RequestCommit.cs
@@ -9,12 +9,12 @@ namespace Stratum.Body.Request
         public static CommitRequest getCommitRequest(DateTime? Until, DateTime? Since, string? Author, string? Path, string? Sha)
         {
             /*
-                An array of Issue parameters for the next call to the GitHub API (dateParams).
+                An array of Commit parameters for the next call to the GitHub API (dateParams).
                 In the appropriate order:, until date, since date.
              */
 
             /*
-                An array of Issue parameters for the next call to the GitHub API (commitParams).
+                An array of Commit parameters for the next call to the GitHub API (commitParams).
                 In the appropriate order: commit's author, commit's path, commit's SHA.
              */
 
